69da92ba601b75bb522e9e7129d85a25c50e3c68
AnaCode05 <ana.isakov05@gmail.com>
2025-05-13 11:08:29 -0400

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalCarnivalRaces
+{
+    /// <summary>
+    /// Generic Animal class - all animals derive their actions and attributes from this
+    /// </summary>
+    abstract class Animal
+    {
+
+    }
+}
